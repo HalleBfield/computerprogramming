ec369714f2136e48db21341ec61282cab1844047
Scholar <scholar@paradigm.local>
2023-09-12 13:34:42 -0600

    operators

--- operators.py
+++ operators.py
@@ -3,3 +3,49 @@
 def add(num1, num2):
     print(num1 + num2)
 
+add(3, 4)
+
+def subtract(num1, num2):
+    print(num1 - num2)
+
+subtract(3, 4)
+
+def multiply(num1, num2):
+    print(num1 * num2)
+
+multiply(3, 4)
+
+def divide(num1, num2):
+    print(num1 / num2)
+
+divide(3, 4)
+
+# <, >, <=, >=, ==, !=
+
+first = 6 > 5
+print(first)
+
+second = 6 < 5
+print(second)
+
+third = 6 <= 5
+print(third)
+
+fourth = 6 >= 5
+print(fourth)
+
+fifth = 6 == 5
+print(fifth)
+
+sixth = 6 != 5
+print(sixth)
+
+seventh = 6 > 4 and 23 < 36
+print(seventh)
+
+eight = 7 > 9 or 5 > 3
+print (eight)
+ninth = 64 < 80
+print(not ninth)
+
+
