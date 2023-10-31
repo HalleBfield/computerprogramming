303d80640757eda43836bff44a37681089074a44
Scholar <scholar@paradigm.local>
2023-10-31 13:34:33 -0600

    interview

---
+++ interview.py
@@ -0,0 +1,29 @@
+# Things you can ask for can include:
+
+# Name
+# Type of programming they're interested in
+# Years of experience
+# Their desired salary
+# Other interview-type questions
+# You must include:
+
+# A total of 10 questions
+# 3 questions must require a correct answer to move on
+# Variables used to save the user's information
+# Professional language throughout
+# A recap of the interview using formatted string literals
+
+
+name = input(" What is your name? ")
+
+ready = input(" Hello {name} Are you ready to start the interview?")
+                 
+Position = input(" What type of position are you interested in? ")
+
+experience = input("How many years of experience do you have? ")
+
+slary = input("What is your desired salary? ")
+
+coding = input("What coding laguages do you know? ")
+
+print(f"Thank you {name} for attending this interview. Let's recap what we went over. Does this sound correct? ")
